70f2801732e289857d1b2ad53aebde70b8c3c6c6
AJ <avjmail@gmail.com>
2017-01-14 19:48:34 +0200

    cleanup

--- src/footballstats/Controllers/GamesController.cs
+++ src/footballstats/Controllers/GamesController.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using footballstats.Data;
 using footballstats.Models;
@@ -176,47 +174,29 @@ namespace footballstats.Controllers
         [HttpPost]
         public async Task<IActionResult> Upload(ICollection<IFormFile> files)
         {
-            var fileContent = String.Empty;
             foreach (var file in files)
             {
                 if (file.Length > 0)
                 {
                     using (var fileStream = new StreamReader(file.OpenReadStream()))
                     {
-                        GameRecord gameRecord;
-                        fileContent = fileStream.ReadToEnd();
-                        gameRecord = JsonConvert.DeserializeObject<GameRecord>(fileContent);
+                        var gameRecord = JsonConvert.DeserializeObject<GameRecord>(fileStream.ReadToEnd());
 
                         if (ModelState.IsValid)
                         {
-                            //foreach (var game in games)
-                            //{
-                            //var refereeExists = _context
-                            //    .Referee
-                            //    .Any(r =>
-                            //        r.Firstname == game.Referee.Firstname &&
-                            //        r.Lastname == game.Referee.Lastname);
+                            var gameExists = _context.Record.Any(g =>
+                                g.Spele.Date == gameRecord.Spele.Date &&
+                                g.Spele.Place == gameRecord.Spele.Place &&
+                                g.Spele.Teams.All(t => 
+                                    gameRecord.Spele.Teams.Any(tx => 
+                                        tx.Title == t.Title)));
 
-                            //if (!refereeExists)
-                            //{
-                            //    _context.Add(game.Referee);
-                            //}
-
-                            ////todo: add side Refereees 
-                            //var gameExists = _context.Game.Any(g =>
-                            //    g.Date == game.Date &&
-                            //    g.Place == game.Place);
-
-
-                            //if (!gameExists)
-                            //{
-                            _context.Add(gameRecord);
-                            //_context.Add(game.Spele); //?
-                            //}
-                            //}
+                            if (!gameExists)
+                            {
+                                _context.Add(gameRecord);
+                            }
 
                             await _context.SaveChangesAsync();
-
                         }
                     }
                 }
