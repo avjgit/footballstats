5579429d15013e50562e5fee40dd3fa8b994e9fa
AJ <avjmail@gmail.com>
2017-01-15 15:41:21 +0200

    Storing referee

--- src/footballstats/Utils/Repository.cs
+++ src/footballstats/Utils/Repository.cs
@@ -10,25 +10,62 @@ namespace footballstats.Utils
 {
     public class Repository
     {
+        ApplicationDbContext _context;
 
         public Repository(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        public void Save(Game game)
         {
+            if (GameExists(game))
                 return;
+
+            //var lineReferees = game.LineReferees;
+            //game.LineReferees = new List<Referee>();
+            //foreach (var referee in lineReferees)
+            //    game.LineReferees.Add(GetOrCreate(referee));
+
+            game.MainReferee = GetOrCreate(game.MainReferee);
+
+            //var composedGame = Create(game);
 
             _context.Add(game);
         }
+        
+        public void DetachAll()
+        {
+            foreach (var dbEntityEntry in _context.ChangeTracker.Entries())
+                if (dbEntityEntry.Entity != null)
+                    dbEntityEntry.State = EntityState.Detached;
+        }
 
-        public bool Exists(Game json) =>
-            _context.Game.Any(g =>  
+        public Referee GetOrCreate(Referee r) =>
+            _context
+                .Referee
+                .AsNoTracking()
+                .FirstOrDefault(x =>
+                x.Firstname == r.Firstname &&
+                x.Lastname == r.Lastname)
+            ?? r;
+
+
+        public bool GameExists(Game json) =>
+            _context.Game.Any(g =>
                 json.Date == g.Date &&
                 json.Place == g.Place &&
                 json.Teams.All(jsonTeam => g.Teams.Any(
                     dbteam => dbteam.Title == jsonTeam.Title)));
 
+        //public Game Create(Game json) => new Game
+        //{
+        //    Date = json.Date,
+        //    Place = json.Place,
+        //    Spectators = json.Spectators,
+        //    LineReferees = json.LineReferees,
+        //    MainReferee = json.MainReferee
+        //};
 
     }
 }
